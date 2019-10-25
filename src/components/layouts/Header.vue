aef1d92c440868ad5d1f11bfb89a978975d45239
her-cat <i@her-cat.com>
2019-10-25 18:26:44 +0800

    顶部导航栏

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,105 +1,53 @@
 <template>
-<div id="app">
-    <div class="layout">
-        <Layout>
-            <Header>
-                <Menu mode="horizontal" theme="primary" active-name="1">
-                    <div class="layout-logo"></div>
-                    <div class="layout-nav">
-                        <MenuItem name="1">
-                            <Icon type="ios-navigate"></Icon>
-                            Item 1
-                        </MenuItem>
-                        <MenuItem name="2">
-                            <Icon type="ios-keypad"></Icon>
-                            Item 2
-                        </MenuItem>
-                        <MenuItem name="3">
-                            <Icon type="ios-analytics"></Icon>
-                            Item 3
-                        </MenuItem>
-                        <MenuItem name="4">
-                            <Icon type="ios-paper"></Icon>
-                            Item 4
-                        </MenuItem>
-                    </div>
-                </Menu>
-            </Header>
-                <Breadcrumb :style="{margin: '16px 0'}">
-                    <BreadcrumbItem>Home</BreadcrumbItem>
-                    <BreadcrumbItem>Components</BreadcrumbItem>
-                    <BreadcrumbItem>Layout</BreadcrumbItem>
-                </Breadcrumb>
-                <Content :style="{padding: '24px 0', minHeight: '280px', background: '#fff'}">
-                    <Layout>
-                        <Sider hide-trigger :style="{background: '#fff'}">
-                            <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-                                <Submenu name="1">
-                                    <template slot="title">
-                                        <Icon type="ios-navigate"></Icon>
-                                        Item 1
-                                    </template>
-                                    <MenuItem name="1-1">Option 1</MenuItem>
-                                    <MenuItem name="1-2">Option 2</MenuItem>
-                                    <MenuItem name="1-3">Option 3</MenuItem>
-                                </Submenu>
-                                <Submenu name="2">
-                                    <template slot="title">
-                                        <Icon type="ios-keypad"></Icon>
-                                        Item 2
-                                    </template>
-                                    <MenuItem name="2-1">Option 1</MenuItem>
-                                    <MenuItem name="2-2">Option 2</MenuItem>
-                                </Submenu>
-                                <Submenu name="3">
-                                    <template slot="title">
-                                        <Icon type="ios-analytics"></Icon>
-                                        Item 3
-                                    </template>
-                                    <MenuItem name="3-1">Option 1</MenuItem>
-                                    <MenuItem name="3-2">Option 2</MenuItem>
-                                </Submenu>
-                            </Menu>
-                        </Sider>
-                        <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
-                            Content
-                        </Content>
-                    </Layout>
-                </Content>
-            </Layout>
-        </Layout>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" @click="toggleNav">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="/" class="navbar-brand">
+                <span class="title">{{ logo.title }}</span>
+            </a>
+        </div>
+        <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+            <ul class="nav navbar-nav">
+                <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+                    <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+                </li>
+            </ul>
+        </div>
     </div>
-</div>
-
+  </div>
 </template>
+
 <script>
-    export default {
-        name: 'App'
+export default {
+  name: 'TheHeader',
+  data () {
+    return {
+      logo: {
+        title: '动弹一下'
+      },
+       navList: ['首页', '关于'],
+       activeNavIndex: 0,
+       showCollapsedNav: false
     }
+  },
+  methods: {
+    changeNavIndex(index) {
+      this.activeNavIndex = index
+    },
+    toggleNav() {
+      this.showCollapsedNav = !this.showCollapsedNav
+    }
+  }
+}
 </script>
 
 <style scoped>
-.layout{
-    border: 1px solid #d7dde4;
-    position: relative;
-    border-radius: 4px;
-    overflow: hidden;
-}
-.layout-logo{
-    width: 100px;
-    height: 30px;
-    border-radius: 3px;
-    float: left;
-    position: relative;
-    top: 15px;
-    left: 20px;
-}
-.layout-nav{
-    width: 420px;
-    margin: 0 auto;
-    margin-right: 20px;
-}
-.layout-footer-center{
-    text-align: center;
-}
+.title { color: gray;}
+.navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
 </style>
