6a011fe96f6df144358d55f7b0d52db5c6c82734
her-cat <i@her-cat.com>
2019-10-25 18:51:23 +0800

    添加登陆注册按钮

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="wrap">
-    <Header/>
+    <Header />
   </div>
 </template>
 
--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,53 +1,70 @@
 <template>
   <div class="navbar navbar-default topnav">
     <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" @click="toggleNav">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a href="/" class="navbar-brand">
-                <span class="title">{{ logo.title }}</span>
-            </a>
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" @click="toggleNav">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="/"
+           class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+        </a>
+      </div>
+      <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+        <ul class="nav navbar-nav">
+          <li v-for="(item, index) in navList"
+              :key="item"
+              v-bind:class="{ active: index === activeNavIndex }">
+            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+          </li>
+        </ul>
+
+        <!-- 入口组件 -->
+        <div class="navbar-right">
+          <Entry />
         </div>
-        <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
-            <ul class="nav navbar-nav">
-                <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
-                    <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
-                </li>
-            </ul>
-        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Entry from '@/components/layouts/Entry'
+
 export default {
-    name: 'Header',
-    data () {
-        return {
-        logo: {
-            title: '动弹一下'
-        },
-        navList: ['首页', '关于'],
-        activeNavIndex: 0,
-        showCollapsedNav: false
-        }
-    },
-    methods: {
-        changeNavIndex(index) {
-            this.activeNavIndex = index
-        },
-        toggleNav() {
-            this.showCollapsedNav = !this.showCollapsedNav
-        }
+  name: 'Header',
+  components: {
+    Entry
+  },
+  data () {
+    return {
+      logo: {
+        title: '动弹一下'
+      },
+      navList: ['首页', '关于'],
+      activeNavIndex: 0,
+      showCollapsedNav: false
     }
+  },
+  methods: {
+    changeNavIndex (index) {
+      this.activeNavIndex = index
+    },
+    toggleNav () {
+      this.showCollapsedNav = !this.showCollapsedNav
+    }
+  }
 }
 </script>
 
 <style scoped>
-.title { color: gray;}
-.navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
+.title {
+  color: gray;
+}
+.navbar-default .navbar-nav > .active > a {
+  background: rgba(0, 0, 0, 0.03);
+}
 </style>
