ccd3757892a43cfbeb34e306a13759876f7f7e64
kelvinKey <kelvinkeyss@yahoo.com>
2019-11-13 17:45:28 +0800

    初始化详情页面

--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Message from './Message'
 import Slider from './Slider'
 import Kernel from './Kernel'
+import Details from './Details'
 
 const components = {
-  Message,
-  Slider,
-  Kernel
+    Message,
+    Slider,
+    Kernel,
+    Details
 }
 
 for (const [key, value] of Object.entries(components)) {
-  Vue.component(key, value)
+    Vue.component(key, value)
 }
