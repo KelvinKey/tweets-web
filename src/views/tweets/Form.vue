9180893ca158a1af43e7f3ea3a801044eca22043
her-cat <i@her-cat.com>
2019-12-07 23:18:17 +0800

    删除 dist

--- src/views/tweets/Form.vue
+++ src/views/tweets/Form.vue
@@ -8,7 +8,7 @@
       <UploadImage ref="uploadImage" @images-updated="imagesUpdated"/>
     </div>
     <div class="form-group tweet-submit">
-      <div class="action-box">
+      <div class="action-box" v-if="isLogged">
         <EmojiPicker @emoji-selected="emojiSelected"/>
         <div @click="uploadImage">
           <i class="fa fa-picture-o" aria-hidden="true"></i> 图片
