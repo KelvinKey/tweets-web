bfec2b2a54933fe762916111172ec4655cc29796
her-cat <i@her-cat.com>
2019-12-29 16:19:01 +0800

    加载更多功能

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="page-home">
-    <div class="row mt-3">
-      <div class="col-md-3">
-        <HotTopics/>
-        <HotTweets class="visible-lg"/>
-      </div>
-      <div class="col-md-6">
-        <TweetForm @page-changed="handlePageChanged"/>
-        <TweetsList :tweets="tweets" @page-changed="handlePageChanged"/>
-      </div>
-      <div class="col-md-3">
-        <ProfileCard class="visible-lg"/>
+  <LoadMore ref="loadMore" :completed="isCompleted" @changeData="changPage">
+    <div class="page-home">
+      <div class="row mt-3">
+        <div class="col-md-3">
+          <HotTopics/>
+          <HotTweets class="visible-lg"/>
+        </div>
+        <div class="col-md-6">
+          <TweetForm @page-changed="handlePageChanged"/>
+          <TweetsList :tweets="tweets" @page-changed="handlePageChanged"/>
+        </div>
+        <div class="col-md-3">
+          <ProfileCard class="visible-lg"/>
+        </div>
       </div>
     </div>
-  </div>
+  </LoadMore>
 </template>
 <script>
   import TweetsList from '../../components/TweetsList'
@@ -23,10 +25,12 @@
   import HotTweets from '../../components/HotTweets'
   import {isEmpty} from 'lodash'
   import md5 from 'js-md5'
+  import LoadMore from '../../components/LoadMore'
 
   export default {
     name: 'Home',
     components: {
+      LoadMore,
       TweetsList,
       TweetForm,
       HotTopics,
@@ -37,18 +41,36 @@
       return {
         topic: {},
         tweets: {},
-        topicName: ''
+        topicName: '',
+        isCompleted: false
       }
     },
     methods: {
       loadTweets (page = 1) {
+        this.$refs.loadMore.$emit('loading', true)
         let path = isEmpty(this.topicName) ? '' : `topics/${md5(this.topicName)}/`
 
         this.$http
           .get(`${path}tweets?include=user&page=${page}`)
-          .then(tweets => { this.tweets = tweets })
+          .then(tweets => {
+            if (this.tweets.data === undefined) {
+              this.tweets = tweets
+            } else {
+              this.tweets.data = this.tweets.data.concat(tweets.data)
+            }
+
+            this.$refs.loadMore.$emit('loading', false)
+          })
       },
       handlePageChanged (page) {
+        this.loadTweets(page)
+      },
+      changPage (page) {
+        if (page > this.tweets.meta.last_page) {
+          this.isCompleted = true
+          return
+        }
+
         this.loadTweets(page)
       }
     },
